a1f2bbbea0e0d6b4cfbe7b7a8fe773bb4b6c2df2
Marcus Baw <marcusbaw@gmail.com>
2020-06-16 13:58:39 +0100

    add import for reference controller

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,5 +1,5 @@
 from .calculations import form_wrapper, perform_calculations, calculate_velocity_acceleration
 from .chart import create_centile_values, create_data_plots
-from .hello import hello
+from .references import references
 from .temp_test_functions import test_sds_tim_term_heights, tim_tests_preterm
 from .upload import import_excel_sheet, download_excel, prepare_data_as_array_of_measurement_objects
