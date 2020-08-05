d07dea810ffd8aed0d320be98997be2f75d1ee07
atheneheaven <joanne.hatton@rcpch.ac.uk>
2020-08-05 10:08:27 +0100

    Update

--- rcpchgrowth/rcpchgrowth/unit_tests/test_sds_pytest.py
+++ rcpchgrowth/rcpchgrowth/unit_tests/test_sds_pytest.py
@@ -15,16 +15,16 @@ from ..date_calculations import chronological_decimal_age, corrected_decimal_age
     "dec_age, measurement, measure, sex, default_to_youngest, preterm, expected", 
     [
         pytest.param(
-            --0.191649555, 'height', 40.48008, None, True, 'male', 0, id= "Height, Boy, 30Weeks"
+            -0.191649555, 'height', 40.48008, 'male', True, True, 0, id= "Height, Boy, 30Weeks"
          ), 
         pytest.param(
-            -0.191649555, 'weight', 1.436, 'male', None, None, 0,  id= "Weight, Boy, 30Weeks"            
+            -0.191649555, 'weight', 1.436, 'male', True, True, 0,  id= "Weight, Boy, 30Weeks"            
         ),
         pytest.param(
-            0, 'height', 51, 'male', None, None,  0, id="Height, Boy, Term"
+            0, 'height', 51, 'male', True, False,  0, id="Height, Boy, Term"
         ),
         pytest.param(
-            5, 'height', 109.59, 'male', None, None, 0,  id="Height, Boy, 5 years"
+            5, 'height', 109.59, 'male', True, False, 0,  id="Height, Boy, 5 years"
         )
     ],
 )
