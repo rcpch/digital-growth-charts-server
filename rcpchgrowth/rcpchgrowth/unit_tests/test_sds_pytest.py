58c1b730305e4596a69c2e82c0754fe1cfd2fb6e
atheneheaven <joanne.hatton@rcpch.ac.uk>
2020-08-05 09:59:57 +0100

    Update

--- rcpchgrowth/rcpchgrowth/unit_tests/test_sds_pytest.py
+++ rcpchgrowth/rcpchgrowth/unit_tests/test_sds_pytest.py
@@ -12,18 +12,24 @@ from ..date_calculations import chronological_decimal_age, corrected_decimal_age
 
 
 @pytest.mark.parametrize(
-    "dec_age, measurement, measure, sex, expected", 
+    "dec_age, measurement, measure, sex, default_to_youngest, preterm, expected", 
     [
         pytest.param(
-            -0.095824778, 'height', 46.1415, 'male', 0, id= "Height, Boy, 30Weeks"
-        ), 
+            --0.191649555, 'height', 40.48008, None, True, 'male', 0, id= "Height, Boy, 30Weeks"
+         ), 
         pytest.param(
-            0, 'height', 51, 'male', 0, id="Height, Boy, Term"
+            -0.191649555, 'weight', 1.436, 'male', None, None, 0,  id= "Weight, Boy, 30Weeks"            
         ),
+        pytest.param(
+            0, 'height', 51, 'male', None, None,  0, id="Height, Boy, Term"
+        ),
+        pytest.param(
+            5, 'height', 109.59, 'male', None, None, 0,  id="Height, Boy, 5 years"
+        )
     ],
 )
 
 
 
-def test_sds_calulations(dec_age, measurement, measure, sex, expected):
-    assert sds(dec_age, measurement, measure, sex, expected) == expected
+def test_sds_calulations(dec_age, measurement, measure, sex, default_to_youngest, preterm, expected):
+    assert sds(dec_age, measurement, measure, sex, default_to_youngest, preterm) == expected
