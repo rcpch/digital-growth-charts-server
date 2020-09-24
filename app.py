e8609dd658ce7a56d7a9897292be782d30da41a1
Marcus Baw <marcusbaw@gmail.com>
2020-09-24 12:50:25 +0100

    remove session import and some old notes/TODOs relating to the removal of sessions as part of the server's working

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 ### LIBRARY IMPORTS
+from app import app
 from datetime import datetime
-from flask import Flask, render_template, request, flash, redirect, url_for, send_from_directory, make_response, jsonify, session
+from flask import Flask, render_template, request, flash, redirect, url_for, send_from_directory, make_response, jsonify
 from flask_cors import CORS
 from flask_restx import Resource, Api, reqparse
 import json
@@ -27,18 +28,8 @@ else:
 
 ### FLASK
 app = Flask(__name__, static_folder="static")
-from app import app
 CORS(app)
 
-"""
-TODO: TO BE DEPRECATED
-Uses session variables to store form or uploaded unique child data
-These session variables are accessed when user wants to download the calculated data,
-or chart them.
-    session["results"] are the form data entered by the user or uploaded from excel
-    session["serial_data"] is a boolean value flagging if data are single measurements 
-    or serial unique patient data
-"""  
 
 """
 API DEFINITIONS SECTION
@@ -70,7 +61,8 @@ def ukwho_height_weight_ofc_bmi_same_day():
         # check here for all the right query params, if not present raise error
     response = controllers.perform_calculations(
         birth_date=datetime.strptime(request.form["birth_date"], "%Y-%m-%d"),
-        observation_date=datetime.strptime(request.form["observation_date"], "%Y-%m-%d"),
+        observation_date=datetime.strptime(
+            request.form["observation_date"], "%Y-%m-%d"),
         height=float(request.form["height_in_cm"]),
         weight=float(request.form["weight_in_kg"]),
         ofc=float(request.form["head_circ_in_cm"]),
@@ -88,9 +80,10 @@ def ukwho_json_calculation():
     # check here for all the right query params, if not present raise error
     final_calculations = controllers.perform_calculation(
         birth_date=datetime.strptime(request.form["birth_date"], "%Y-%m-%d"),
-        observation_date=datetime.strptime(request.form["observation_date"], "%Y-%m-%d"),
+        observation_date=datetime.strptime(
+            request.form["observation_date"], "%Y-%m-%d"),
         measurement_method=str(request.form["measurement_method"]),
-        observation_value =float(request.form["observation_value"]),
+        observation_value=float(request.form["observation_value"]),
         sex=str(request.form["sex"]),
         gestation_weeks=int(request.form["gestation_weeks"]),
         gestation_days=int(request.form["gestation_days"])
@@ -118,11 +111,12 @@ To amend the instructions please submit a pull request
 """
 @app.route("/growth/ukwho/chart_data", methods=["POST"])
 def chart_data():
-    results=json.loads(request.form["results"])
+    results = json.loads(request.form["results"])
     unique_child = request.form["unique_child"]
     # unique_child = request.args["unique_child"]
     # born preterm flag to pass to charts
-    born_preterm = (results[0]["birth_data"]["gestation_weeks"]!= 0 and results[0]["birth_data"]["gestation_weeks"] < 37)
+    born_preterm = (results[0]["birth_data"]["gestation_weeks"]
+                    != 0 and results[0]["birth_data"]["gestation_weeks"] < 37)
     if unique_child == "true":
         #data are serial data points for a single child
         # Prepare data from plotting
@@ -136,7 +130,8 @@ def chart_data():
         # Retrieve sex of child to select correct centile charts
         sex = results[0]["birth_data"]["sex"]
     # Create Centile Charts
-    centiles = controllers.create_centile_values(sex, born_preterm=born_preterm)
+    centiles = controllers.create_centile_values(
+        sex, born_preterm=born_preterm)
     return jsonify({
         "sex": sex,
         "child_data": child_data,
@@ -170,14 +165,14 @@ Returns a series of fictional data for a child
 @app.route("growth/utilities/create_fictional_child_measurements", methods=["POST"])
 def utilities_create_fictional_child_measurements():
     fictional_child_data = controllers.generate_fictional_data(
-        drift_amount = float(request.form["drift_amount"]),
-        intervals = int(request.form["intervals"]),
-        interval_type = request.form["interval_type"],
-        measurement_requested = request.form["measurement_requested"],
-        number_of_measurements = int(request.form["number_of_measurements"]),
-        sex = request.form["sex"],
-        starting_age = float(request.form["starting_age"]),
-        starting_sds = float(request.form["starting_sds"])
+        drift_amount=float(request.form["drift_amount"]),
+        intervals=int(request.form["intervals"]),
+        interval_type=request.form["interval_type"],
+        measurement_requested=request.form["measurement_requested"],
+        number_of_measurements=int(request.form["number_of_measurements"]),
+        sex=request.form["sex"],
+        starting_age=float(request.form["starting_age"]),
+        starting_sds=float(request.form["starting_sds"])
     )
     return jsonify(fictional_child_data)
 
