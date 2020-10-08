b893daa147fa5c85ef9a7e0ca761f2c6ae97e9a5
Marcus Baw <marcusbaw@gmail.com>
2020-10-08 19:54:23 +0100

    slight refactor

--- controllers/calculations.py
+++ controllers/calculations.py
@@ -4,6 +4,8 @@ from rcpchgrowth.rcpchgrowth.date_calculations import chronological_decimal_age
 from rcpchgrowth.rcpchgrowth.dynamic_growth import velocity, acceleration
 
 # this wrapper changes form data to the right kwargs for the calculation function
+
+
 def form_wrapper(form):
     return perform_calculations(
         birth_date=form.birth_date.data,
@@ -21,53 +23,53 @@ def form_wrapper(form):
 # https://github.com/python/typing/issues/433#issuecomment-302491149
 # This function takes up to 3 measurements performed on the same day and returns SDS and centiles on 4 (BMI is itself a calculated value)
 # It is being deprecated - it does not accept a BMI, nor does it a calculate one from heights and weights
-def perform_calculations(*, 
-        birth_date: date,
-        observation_date: date,
-        height: float,
-        weight: float,
-        ofc: float,
-        sex: str,
-        gestation_weeks: int,
-        gestation_days:int):
+def perform_calculations(*,
+                         birth_date: date,
+                         observation_date: date,
+                         height: float,
+                         weight: float,
+                         ofc: float,
+                         sex: str,
+                         gestation_weeks: int,
+                         gestation_days: int):
 
     array_of_measurement_objects = []
     if height:
         # measurement_type = Measurement_Type("height", height=height)
         measurement_object = Measurement(
-            sex=sex, 
-            birth_date=birth_date, 
-            observation_date=observation_date, 
+            sex=sex,
+            birth_date=birth_date,
+            observation_date=observation_date,
             measurement_method="height",
             observation_value=height,
-            gestation_weeks=gestation_weeks, 
-            gestation_days=gestation_days, 
+            gestation_weeks=gestation_weeks,
+            gestation_days=gestation_days,
             default_to_youngest_reference=False)
         array_of_measurement_objects.append(measurement_object.measurement)
     if weight:
         measurement_object = Measurement(
-            sex=sex, 
-            birth_date=birth_date, 
-            observation_date=observation_date, 
+            sex=sex,
+            birth_date=birth_date,
+            observation_date=observation_date,
             measurement_method="weight",
             observation_value=weight,
-            gestation_weeks=gestation_weeks, 
-            gestation_days=gestation_days, 
+            gestation_weeks=gestation_weeks,
+            gestation_days=gestation_days,
             default_to_youngest_reference=False)
         array_of_measurement_objects.append(measurement_object.measurement)
-    # if height and weight: 
+    # if height and weight:
     #     measurement_type = Measurement_Type("bmi", height=height, weight=weight)
     #     measurement_object = Measurement(sex=sex, birth_date=birth_date, observation_date=observation_date, measurement_type=measurement_type, gestation_weeks=gestation_weeks, gestation_days=gestation_days, default_to_youngest_reference=False)
     #     array_of_measurement_objects.append(measurement_object.measurement)
     if ofc:
         measurement_object = Measurement(
-            sex=sex, 
-            birth_date=birth_date, 
-            observation_date=observation_date, 
+            sex=sex,
+            birth_date=birth_date,
+            observation_date=observation_date,
             measurement_method="ofc",
             observation_value=ofc,
-            gestation_weeks=gestation_weeks, 
-            gestation_days=gestation_days, 
+            gestation_weeks=gestation_weeks,
+            gestation_days=gestation_days,
             default_to_youngest_reference=False)
         array_of_measurement_objects.append(measurement_object.measurement)
     return array_of_measurement_objects
@@ -75,30 +77,30 @@ def perform_calculations(*,
 # This function takes a measurement_method as a string ('height', 'weight', 'bmi' or 'ofc') and returns a Measurement object with the calculated values.
 # Note that BMI must be provided already calculated as a parameter to this function.
 # Note that measurement_method is a string passed by the form and is distinct from Measurement_Type which is a class relating to the same
+
+
 def perform_calculation(*,
-    birth_date: date,
-    observation_date: date,
-    measurement_method: str,
-    observation_value: float,
-    sex: str,
-    gestation_weeks: int,
-    gestation_days: int):
+                        birth_date: date,
+                        observation_date: date,
+                        measurement_method: str,
+                        observation_value: float,
+                        sex: str,
+                        gestation_weeks: int,
+                        gestation_days: int):
 
-    array_of_measurement_objects = []
+    measurement_object = Measurement(
+        sex=sex,
+        birth_date=birth_date,
+        observation_date=observation_date,
+        measurement_method=measurement_method,
+        observation_value=float(observation_value),
+        gestation_weeks=gestation_weeks,
+        gestation_days=gestation_days,
+        default_to_youngest_reference=False
+    )
 
-    # if measurement_method == "height":
-    #     measurement_type = Measurement_Type("height", measurement_value=float(observation_value))
-    # if measurement_method == "weight":
-    #     measurement_type = Measurement_Type("weight", measurement_value=float(observation_value))
-    # if measurement_method == "bmi": 
-    #     measurement_type = Measurement_Type("bmi", measurement_value=float(observation_value))
-    # if measurement_method == "ofc":
-    #     measurement_type = Measurement_Type("ofc", measurement_value=float(observation_value))
-    measurement_object = Measurement(sex=sex, birth_date=birth_date, observation_date=observation_date, measurement_method=measurement_method, observation_value=float(observation_value), gestation_weeks=gestation_weeks, gestation_days=gestation_days, default_to_youngest_reference=False)
-    
-    ## return measurement object as an array
-    array_of_measurement_objects.append(measurement_object.measurement)
-    return array_of_measurement_objects
+    return measurement_object.measurement
+
 
 def calculate_velocity_acceleration(data):
     height_velocity = velocity("height", data)
