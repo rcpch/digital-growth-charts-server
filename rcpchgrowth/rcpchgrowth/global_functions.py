00c3487042c0654a74a138685e20586ae93ed438
Simon Chapman <eatyourpeasapps@gmail.com>
2020-11-21 22:26:44 +0000

    create_chart function agnostic of reference implemented. another step towards We should be able to produce growth charts for Down's Syndrome #41

--- rcpchgrowth/rcpchgrowth/global_functions.py
+++ rcpchgrowth/rcpchgrowth/global_functions.py
@@ -96,30 +96,6 @@ def centile(z_score: float):
     centile = (stats.norm.cdf(z_score) * 100)
     return centile
 
-def sds_value_for_centile_value(centile: float):
-
-    if centile == 0.4:
-        return -2.0 - (2/3)
-    elif centile == 2:
-        return -2.0
-    elif centile == 9:
-        return -1 - (1/3)
-    elif centile == 25:
-        return 0 - (2/3)
-    elif centile == 50:
-        return 0
-    elif centile == 75:
-        return 2/3
-    elif centile == 91:
-        return 1 + (1/3)
-    elif centile == 98:
-        return 2.0
-    elif centile == 99.6:
-        return 2 + (2/3)
-    else:
-        #error
-        raise LookupError("SDS could not be calculated from Centile supplied")
-
 def measurement_for_z(z: float, l: float, m:float, s:float)->float:
 
     """
@@ -293,3 +269,106 @@ def lms_value_array_for_measurement_for_reference(
     else:
         raise ValueError("Incorrect reference supplied")
     return lms_value_array_for_measurement
+
+
+def generate_centile(z: float, centile: float, measurement_method: str, sex:str, lms_array_for_measurement: list, reference: str)->list:
+
+    """
+    Generates a centile curve for a given reference. 
+    Takes the z-score equivalent of the centile, the centile to be used as a label, the sex and measurement method.
+    """
+
+    min_age = lms_array_for_measurement[0]["decimal_age"]
+    max_age = lms_array_for_measurement[-1]["decimal_age"]
+
+    # ages=[1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0, 4.2, 4.4, 4.6, 4.8, 5.0, 5.2, 5.4, 5.6, 5.8, 6.0, 6.2, 6.4, 6.6, 6.8, 7.0, 7.2, 7.4, 7.6, 7.8, 8.0, 8.2, 8.4, 8.6, 8.8, 9.0, 9.2, 9.4, 9.6, 9.8, 10.0, 10.2, 10.4, 10.6, 10.8, 11.0, 11.2, 11.4, 11.6, 11.8, 12.0, 12.2, 12.4, 12.6, 12.8, 13.0, 13.2, 13.4, 13.6, 13.8, 14.0, 14.2, 14.4, 14.6, 14.8, 15.0, 15.2, 15.4, 15.6, 15.8, 16.0, 16.2, 16.4, 16.6, 16.8, 17.0, 17.2, 17.4, 17.6, 17.8, 18.0, 18.2, 18.4, 18.6, 18.8, 19.0, 19.2, 19.4, 19.6, 19.8]
+    centile_measurements=[]
+    age = min_age
+    while age < max_age:
+        # loop through the reference in steps of 0.1y
+        measurement = measurement_from_sds(reference=reference, measurement_method=measurement_method, requested_sds=z, sex=sex, age=age)
+        ## creates a data point
+        value = {
+            "label": centile,
+            "x": age,
+            "y": measurement
+        }
+        centile_measurements.append(value)
+        age += 0.1
+    return {
+        "sds": z,
+        "centile": centile,
+        "turner_data": centile_measurements
+    }
+
+def create_chart(reference: str, measurement_method:str, sex: str, age: float, born_preterm=False):
+    centile_array = [0.4, 2, 9, 25, 50, 75, 91, 98, 99.6]
+    try:
+        lms_value_array_for_measurement = lms_value_array_for_measurement_for_reference(reference=reference, age=age, measurement_method=measurement_method, sex=sex, born_preterm=born_preterm)
+    except LookupError as err:
+        print(err)
+        return
+    chart_values=[]
+    for index, centile in enumerate(centile_array):
+        z=sds_value_for_centile_value(centile=centile)
+        centile_line = generate_centile(z=z, centile=centile, measurement_method=measurement_method, sex=sex, lms_array_for_measurement=lms_value_array_for_measurement, reference=reference)
+        chart_values.append(centile_line)
+    return {"data": chart_values, "key": measurement_method}
+
+def sds_value_for_centile_value(centile: float):
+    
+    if centile == 0.4:
+        return -2.0 - (2/3)
+    elif centile == 2:
+        return -2.0
+    elif centile == 9:
+        return -1 - (1/3)
+    elif centile == 25:
+        return 0 - (2/3)
+    elif centile == 50:
+        return 0
+    elif centile == 75:
+        return 2/3
+    elif centile == 91:
+        return 1 + (1/3)
+    elif centile == 98:
+        return 2.0
+    elif centile == 99.6:
+        return 2 + (2/3)
+    else:
+        #error
+        raise LookupError("SDS could not be calculated from Centile supplied")
+
+    """
+    Return object structure
+
+    [
+        {
+            childData: [
+                {
+                    x: 9.415, `this is the age of the child at date of measurement
+                    y: 120 `this is the observation value
+                }
+
+            ],
+            data: [
+                {
+                    sds: -2.666666,
+                    uk90_child_data:[.....],
+                    uk90_preterm_data: [...],
+                    who_child_data: [...],
+                    who_infant_data: [
+                        {
+                            label: 0.4, `this is the centile
+                            x: 4, `this is the decimal age
+                            y: 91.535  `this is the measurement
+                        }
+                    ]
+                }
+            ],
+            key: "height"
+        },
+        ... repeat for weight, bmi, ofc, based on which measurements supplied. If only height data supplied, only height centile data returned
+    ]
+
+    """
