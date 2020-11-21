504c0686a7a01e2e774c65fe33dc74b9e0ac0933
Simon Chapman <eatyourpeasapps@gmail.com>
2020-11-21 19:05:26 +0000

    Percentage median BMI now fixed Includes error handling for all references Fixes #114

--- rcpchgrowth/rcpchgrowth/trisomy_21.py
+++ rcpchgrowth/rcpchgrowth/trisomy_21.py
@@ -25,47 +25,11 @@ with open(T21_DATA) as json_file:
             T21_DATA = json.load(json_file)
             json_file.close()
 
-# def t21_sds_calculation(
-#     age: float,
-#     measurement_method: str,
-#     observation_value: float,
-#     sex: str,
-#     )->float:
-
-#     """
-#     The is the caller function to calculate an SDS for a child specifically using the Down's syndrome dataset.
-    
-#     """
-
-#     try:
-#         selected_reference = T21_DATA
-#     except: # there is no reference for the age supplied
-#         return ValueError("The Down's syndrom reference cannot be found.")
-
-#     # Check that the measurement requested has reference data at that age
-#     if reference_data_absent(
-#         age=age, 
-#         measurement_method=measurement_method, 
-#         sex=sex):
-#         return ValueError("There is no reference data for this measurement at this age")
-
-#     # fetch the LMS values for the requested measurement
-#     lms_value_array_for_measurement = selected_reference["measurement"][measurement_method][sex]
-
-#     # get LMS values from the reference: check for age match, interpolate if none
-#     lms = fetch_lms(age=age, lms_value_array_for_measurement=lms_value_array_for_measurement)
-#     l = lms["l"]
-#     m = lms["m"]
-#     s = lms["s"]
-#     ## calculate the SDS from the L, M and S values
-
-#     return z_score(l=l, m=m, s=s, observation=observation_value)
-
 def reference_data_absent( 
         age: float,
         measurement_method: str,
         sex: str
-    ) -> bool:
+    ) -> (bool, str):
     """
     Helper function.
     Returns boolean
@@ -78,69 +42,31 @@ def reference_data_absent(
     """
 
     if age < 0: # lower threshold of T21 data
-        return True
+        return True, "Trisomy 21 reference data does not include prematurity."
     
     if age > TWENTY_YEARS: # upper threshold of T21 data
         return True
         
     elif measurement_method == "bmi" and age > 18.82:
-        return True
+        return True, "Trisomy BMI reference data does not exist > 18.82 y"
     
     elif measurement_method == "ofc":
         if age > EIGHTEEN_YEARS:
-            return True
+            return True, "Trisomy head circumference reference data does not exist > 18 y"
         else:
-            return False
+            return False, ""
     else:
-        return False
-
-# def measurement_from_sds(
-#     measurement_method: str,  
-#     requested_sds: float,  
-#     sex: str,  
-#     age: float,
-#     ) -> float:
-#     """
-#     Public method
-#     Returns the measurement value from a given SDS.
-#     Parameters are: 
-#         measurement_method (type of observation) ["height", "weight", "bmi", "ofc"]
-#         decimal age (corrected or chronological),
-#         requested_sds
-#         sex (a standard string) ["male" or "female"]
-#         born_preterm: a boolean flag to track whether to use UK90 premature data or UK90-WHO term data for 37-42 weeks
-
-#     Centile to SDS Conversion for Chart lines (2/3 of an SDS)
-#     0.4th -2.67
-#     2nd -2.00
-#     9th -1.33
-#     25th -0.67
-#     50th 0
-#     75th 0.67
-#     91st 1.33
-#     98th 2.00
-#     99.6th 2.67
-#     """
-
-#     # fetch the LMS values for the requested measurement
-#     lms_value_array_for_measurement = trisomy_21_lms_array_for_measurement_and_sex(measurement_method=measurement_method, sex=sex)
-
-#     # test reference data exists for age
-#     if reference_data_absent(age=age, measurement_method=measurement_method, sex=sex):
-#         print(f'There is no reference data for this measurement at {age} years.')
-#         return None
-#     else:
-#         # get LMS values from the reference: check for age match, interpolate if none
-#         lms = fetch_lms(age=age, lms_value_array_for_measurement=lms_value_array_for_measurement)
-#         l = lms["l"]
-#         m = lms["m"]
-#         s = lms["s"]
-
-#         observation_value = measurement_for_z(z=requested_sds, l=l, m=m, s=s)
-#         return observation_value
+        return False, ""
 
 def trisomy_21_lms_array_for_measurement_and_sex(
         measurement_method: str,
-        sex: str
+        sex: str,
+        age: float
     ):
-    return T21_DATA["measurement"][measurement_method][sex]
+
+    data_invalid, data_error = reference_data_absent(age=age, measurement_method=measurement_method, sex=sex)
+
+    if data_invalid:
+        raise LookupError(data_error)
+    else:
+        return T21_DATA["measurement"][measurement_method][sex]
--- rcpchgrowth/rcpchgrowth/turner.py
+++ rcpchgrowth/rcpchgrowth/turner.py
@@ -25,55 +25,29 @@ with open(TURNER_DATA) as json_file:
             TURNER_DATA = json.load(json_file)
             json_file.close()
 
-# def turner_sds_calculation(
-#     age: float,
-#     measurement_method: str,
-#     observation_value: float,
-#     sex: str,
-#     )->float:
-
-#     """
-#     The is the caller function to calculate an SDS for a child specifically using the Turner"s syndrome dataset.
-    
-#     """
-
-#     # Check that the measurement requested has reference data at that age
-#     if reference_data_absent(
-#         age=age, 
-#         measurement_method=measurement_method, 
-#         sex=sex):
-#         print ("There is no reference data for this measurement.")
-#         return None
-
-#     # fetch the LMS values for the requested measurement
-#     lms_value_array_for_measurement = turner_lms_array_for_measurement_and_sex(measurement_method=measurement_method, sex=sex)
-
-#     # get LMS values from the reference: check for age match, interpolate if none
-#     lms = fetch_lms(age=age, lms_value_array_for_measurement=lms_value_array_for_measurement)
-#     l = lms["l"]
-#     m = lms["m"]
-#     s = lms["s"]
-#     ## calculate the SDS from the L, M and S values
-
-#     return z_score(l=l, m=m, s=s, observation=observation_value)
-
 def turner_lms_array_for_measurement_and_sex(
         measurement_method: str,    
         sex: str,  
+        age: float
     ):
+
+    invalid_data, data_error = reference_data_absent(age=age, measurement_method=measurement_method, sex=sex)
+
+    if invalid_data:
+        raise LookupError(data_error)
+
     # Get the Turner reference data
     try:
         return TURNER_DATA["measurement"][measurement_method][sex]
     except: # there is no reference for the age supplied
-        print("The Turner's syndrome reference cannot be found.")
-        return None
+        raise LookupError("The Turner's syndrome reference cannot be found.")
     
 
 def reference_data_absent( 
         age: float,
         measurement_method: str,
         sex: str
-    ) -> bool:
+    ) -> (bool, str):
     """
     Helper function.
     Returns boolean
@@ -85,59 +59,20 @@ def reference_data_absent(
     """
 
     if age < 1: # lower threshold of Turner data
-        return True
+        return True, 'There is no reference data below 1 year.'
     elif age > TWENTY_YEARS: # upper threshold of Turner data
-        return True
+        return True, "There is no reference data above 20 years."
     elif measurement_method=="weight" or measurement_method=="bmi" or measurement_method=="ofc":
-        return True
+        text_string = measurement_method
+        if measurement_method == "bmi":
+            text_string = "BMI (body mass index)"
+        elif measurement_method == "ofc":
+            text_string = "head circumference"
+        return True, f"There is no reference data for {text_string}."
     elif sex == "male":
-        return True
+        return True, f"Turner's syndrome only affects girls and women."
     else:
-        return False
-
-# def turner_measurement_from_sds(
-#     measurement_method: str,  
-#     requested_sds: float,  
-#     sex: str,  
-#     age: float,
-#     ) -> float:
-#     """
-#     Public method
-#     Returns the measurement value from a given SDS.
-#     Parameters are: 
-#         measurement_method (type of observation) ["height", "weight", "bmi", "ofc"]
-#         decimal age (corrected or chronological),
-#         requested_sds
-#         sex (a standard string) ["male" or "female"]
-#         born_preterm: a boolean flag to track whether to use UK90 premature data or UK90-WHO term data for 37-42 weeks
-
-#     Centile to SDS Conversion for Chart lines (2/3 of an SDS)
-#     0.4th -2.67
-#     2nd -2.00
-#     9th -1.33
-#     25th -0.67
-#     50th 0
-#     75th 0.67
-#     91st 1.33
-#     98th 2.00
-#     99.6th 2.67
-#     """
-
-#     # fetch the LMS values for the requested measurement
-#     lms_value_array_for_measurement = TURNER_DATA["measurement"][measurement_method][sex]
-
-#     # test reference data exists for age
-#     if reference_data_absent(age=age, measurement_method=measurement_method, sex=sex):
-#         return ValueError(f"There is no reference data for this measurement at {age} years.")
-#     else:
-#         # get LMS values from the reference: check for age match, interpolate if none
-#         lms = fetch_lms(age=age, lms_value_array_for_measurement=lms_value_array_for_measurement)
-#         l = lms["l"]
-#         m = lms["m"]
-#         s = lms["s"]
-
-#         observation_value = measurement_for_z(z=requested_sds, l=l, m=m, s=s)
-#         return observation_value
+        return False, "Valid Data"
 
 # def generate_centile(z: float, centile: float, measurement_method: str, sex='female'):
 #     ages=[1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0, 4.2, 4.4, 4.6, 4.8, 5.0, 5.2, 5.4, 5.6, 5.8, 6.0, 6.2, 6.4, 6.6, 6.8, 7.0, 7.2, 7.4, 7.6, 7.8, 8.0, 8.2, 8.4, 8.6, 8.8, 9.0, 9.2, 9.4, 9.6, 9.8, 10.0, 10.2, 10.4, 10.6, 10.8, 11.0, 11.2, 11.4, 11.6, 11.8, 12.0, 12.2, 12.4, 12.6, 12.8, 13.0, 13.2, 13.4, 13.6, 13.8, 14.0, 14.2, 14.4, 14.6, 14.8, 15.0, 15.2, 15.4, 15.6, 15.8, 16.0, 16.2, 16.4, 16.6, 16.8, 17.0, 17.2, 17.4, 17.6, 17.8, 18.0, 18.2, 18.4, 18.6, 18.8, 19.0, 19.2, 19.4, 19.6, 19.8]
