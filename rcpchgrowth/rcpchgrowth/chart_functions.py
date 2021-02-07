59484ae799d3b753d4fbda3f0924ecd4d12f6a8e
Simon Chapman <eatyourpeasapps@gmail.com>
2021-02-07 15:43:33 +0000

    loose ends

--- rcpchgrowth/rcpchgrowth/chart_functions.py
+++ rcpchgrowth/rcpchgrowth/chart_functions.py
@@ -92,16 +92,27 @@ def create_plottable_child_data(child_results_array):
 
     [
         heights: [
-            {
-                x: 9.415, `this is the age of the child at date of measurement in decimal years
+            [{
+                x: 9.415, `this is the corrected age of the child at date of measurement in decimal years
                 y: 120 `this is the observation value - the units will be added in the client
-                "centile_band": 'You childs height is between the 75th and 91st centiles' `a text advice string for labelling,
-                "centile_value": 86 `centile number value - reported but not used: the project board do not like exact centile numbers,
+                "centile_band": 'Your child's height is between the 75th and 91st centiles' `a text advice string for labelling - corrected,
+                "centile_value": 86 `centile number value - reported but not used: the project board do not like exact centile numbers - corrected,
                 "age_type": "corrected_age", `this is a flag to differentiate the two ages for the same observation_value
                 "calendar_age": "9 years and 4 months" `this is the calendar age for labelling
                 "corrected_gestational_weeks": 23 `the corrected gestational age if relevant for labelling
                 "corrected_gestational_days": 1 `the corrected gestational age if relevant for labelling
             }
+            {
+                x: 9.415, `this is the chronological age of the child at date of measurement in decimal years
+                y: 120 `this is the observation value - the units will be added in the client
+                "centile_band": 'Your child's height is between the 75th and 91st centiles' `a text advice string for labelling - based on chronological age,
+                "centile_value": 86 `centile number value - reported but not used: the project board do not like exact centile numbers - based on chronological age, 
+                "age_type": "corrected_age", `this is a flag to differentiate the two ages for the same observation_value
+                "calendar_age": "9 years and 4 months" `this is the calendar age for labelling
+                "corrected_gestational_weeks": 23 `the corrected gestational age if relevant for labelling
+                "corrected_gestational_days": 1 `the corrected gestational age if relevant for labelling
+            }
+            ]
         ],
         height_sds: [
                 x: 9.415, `this is the age of the child at date of measurement in decimal years
