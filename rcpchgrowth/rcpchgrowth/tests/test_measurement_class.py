c38ed34f47d1382402561d15d893bb4db8a95b74
Marcus Baw <marcusbaw@gmail.com>
2021-04-21 13:21:53 +0100

    16k test points ahoy

--- rcpchgrowth/rcpchgrowth/tests/test_measurement_class.py
+++ rcpchgrowth/rcpchgrowth/tests/test_measurement_class.py
@@ -2,6 +2,7 @@
 from datetime import datetime
 import json
 import os
+from pprint import pprint
 
 # third-party imports
 import pytest
@@ -26,80 +27,49 @@ def load_valid_data_set():
         return json.load(f)
 
 
-# @pytest.mark.parametrize("line", load_valid_data_set())
-# def test_measurement_class_with_valid_data_set(line):
-#     print(line)
-#     """
-#     Test which iterates through a JSON file of fictional children and known calculated (TC via R)
-#       correct SDS values. Compares the output of the Measurement.measurement method with the known
-#       correct value.
-#     """
-
-#     measurement_object = Measurement(
-#         sex=str(line["sex"]),
-#         birth_date=datetime.strptime(line["birth_date"], "%Y-%m-%d"),
-#         observation_date=datetime.strptime(
-#             line["observation_date"], "%Y-%m-%d"),
-#         measurement_method=str(line["measurement_method"]),
-#         observation_value=float(line["observation_value"]),
-#         gestation_weeks=int(line["gestation_weeks"]),
-#         gestation_days=int(line["gestation_days"]),
-#         reference="uk-who"
-#     )
-
-#     rcpchgrowth_result = measurement_object.measurement["measurement_calculated_values"]['corrected_sds']
-#     tim_cole_r_result = line["SDS"]
-
-#     # comparison using absolute tolerance (not relative)
-#     assert rcpchgrowth_result == pytest.approx(tim_cole_r_result, abs=ACCURACY)
-
-# @pytest.mark.parametrize("line", load_valid_data_set())
-# def test_corrected_sds_calculation(line):
-#     if line["observation_value"]==None or line["corrected_sds"]==None:
-#         return
-#     sds = global_functions.sds_for_measurement("uk-who", float(line["corrected_age"]), str(line["measurement_method"]), float(line["observation_value"]), str(line["sex"]), False)
-#     tim_sds = float(line["corrected_sds"])
-#     assert sds == pytest.approx(tim_sds, abs=ACCURACY)
-
-# @pytest.mark.parametrize("line", load_valid_data_set())
-# def test_chronological_sds_calculation(line):
-#     if line["observation_value"]==None or line["chronological_sds"]==None:
-#         return
-#     sds = global_functions.sds_for_measurement("uk-who", float(line["chronological_age"]), str(line["measurement_method"]), float(line["observation_value"]), str(line["sex"]), False)
-#     tim_sds = float(line["chronological_sds"])
-#     assert sds == pytest.approx(tim_sds, abs=ACCURACY)
-
-# @pytest.mark.parametrize("line", load_valid_data_set())
-# def test_corrected_age_calculation(line):
-#     if line["birth_date"]==None or line["observation_date"]==None:
-#         return
-#     birth_date = datetime.strptime(line["birth_date"], "%d/%m/%Y").date()
-#     observation_date = datetime.strptime(line["observation_date"], "%d/%m/%Y").date()
-#     age = date_calculations.corrected_decimal_age(birth_date, observation_date, int(line["gestation_weeks"]), int(line["gestation_days"]))
-#     tim_age = float(line["corrected_age"])
-#     assert age == pytest.approx(tim_age, abs=ACCURACY)
-
 @pytest.mark.parametrize("line", load_valid_data_set())
-def test_chronological_age_calculation(line):
-    if line["birth_date"] == None or line["observation_date"] == None:
-        return
-    birth_date = datetime.strptime(line["birth_date"], "%d/%m/%Y").date()
-    observation_date = datetime.strptime(
-        line["observation_date"], "%d/%m/%Y").date()
-    age = date_calculations.chronological_decimal_age(
-        birth_date, observation_date)
-    tim_age = float(line["chronological_age"])
-    assert age == pytest.approx(tim_age, abs=ACCURACY)
+def test_measurement_class_ukwho_data(line):
+    """
+    Test which iterates through a JSON file of 4000 fictional children and known calculated (TC via R)
+      correct SDS values. Compares the output of the Measurement.measurement method with the known
+      correct value.
+    """
 
+    # xfail the 121 tests which try to calculate an OFC-SDS where there is no UK-WHO data
+    if line["measurement_method"] == "ofc" and line["chronological_age"] > 17.0:
+        if line["sex"] == "female":
+            pytest.xfail(
+                "UK-WHO OFC data is not available for females over the age of 17.0")
+        elif line["chronological_age"] > 18.0:
+            pytest.xfail(
+                "UK-WHO OFC data is not available for males over the age of 18.0")
 
-# def test_measurement_class_with_invalid_sex_type():
-#     measurement_object = Measurement(
-#         sex="males",
-#         birth_date=datetime.strptime("2020-04-01", "%Y-%m-%d"),
-#         observation_date=datetime.strptime("2020-06-01", "%Y-%m-%d"),
-#         measurement_method="weight",
-#         observation_value=5.0,
-#         gestation_weeks=0,
-#         gestation_days=40,
-#         reference="uk-who"
-#     )
+    # xfail the 2 tests which try to calculate a BMI - SDS where there is no UK - WHO data
+    if line["measurement_method"] == "bmi" and line["corrected_age"] < 0.038329911019849415:
+        pytest.xfail(
+            "UK-WHO BMI data is not available for babies under 2 weeks")
+
+    measurement_object = Measurement(
+        sex=str(line["sex"]),
+        birth_date=datetime.strptime(line["birth_date"], "%d/%m/%Y"),
+        observation_date=datetime.strptime(
+            line["observation_date"], "%d/%m/%Y"),
+        measurement_method=str(line["measurement_method"]),
+        observation_value=float(line["observation_value"]),
+        gestation_weeks=int(line["gestation_weeks"]),
+        gestation_days=int(line["gestation_days"]),
+        reference="uk-who"
+    )
+
+    pprint(vars(measurement_object))
+    pprint(line)
+
+    # RCPCH package calculated value for SDS
+    rcpchgrowth_result = measurement_object.measurement[
+        "measurement_calculated_values"]['corrected_sds']
+
+    # precalculated 'known correct' value from Tim's R package
+    tim_cole_r_result = line["corrected_sds"]
+
+    # comparison using absolute tolerance (not relative)
+    assert rcpchgrowth_result == pytest.approx(tim_cole_r_result, abs=ACCURACY)
