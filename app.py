32665d805530e7b630fe5555620c5940e084ecd8
Simon Chapman <eatyourpeasapps@gmail.com>
2021-06-02 17:34:40 +0100

    quotes fix

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from rcpchgrowth.constants.reference_constants import *
 
 
 ### API VERSION AND COMMIT HASH ###
-version = "2.2.0"
+version = '2.2.0'
 API_SEMANTIC_VERSION =  version # this is set by bump version
 
 # Read saved version info from **saved** JSON APIspec
