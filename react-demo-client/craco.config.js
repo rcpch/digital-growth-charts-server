72e2b852ded9abc6269590934926b89c0c523783
Marcus Baw <marcusbaw@gmail.com>
2021-07-07 20:33:34 +0100

    remove the react app as we are using an iframe now

--- react-demo-client/craco.config.js
+++
@@ -1,9 +0,0 @@
-const { ESLINT_MODES } = require('@craco/craco');
-
-
-module.exports = {
-  plugins: [{ plugin: require("@semantic-ui-react/craco-less") }],
-  eslint: {
-    mode: ESLINT_MODES.file,
-  }
-};
--- react-demo-client/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders RCPCH Growth Charts text", () => {
-  const { getByText } = render(<App />);
-  const textElement = getByText(/Digital Growth Charts Demo/i);
-  expect(textElement).toBeInTheDocument();
-});
