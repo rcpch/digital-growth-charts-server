e6a3dd8021dbb494f1dac46e70a29eaf9cd6de5d
Simon Chapman <eatyourpeasapps@gmail.com>
2021-08-08 22:28:43 +0100

    response object created

--- schemas/response_schema_classes.py
+++ schemas/response_schema_classes.py
@@ -8,4 +8,96 @@ In this file we define or import the response schemas
 # local / rcpch imports
 # from rcpchgrowth.schemas import MeasurementResponseSchema
 
-# def MeasurementResponse
+from datetime import date
+
+from typing import Optional, Literal
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class CorrectedGestationalAge(BaseModel):
+    corrected_gestation_weeks: int
+    corrected_gestation_days: int
+class Comments(BaseModel):
+    clinician_corrected_decimal_age_comment: str
+    lay_corrected_decimal_age_comment: str
+    clinician_chronological_decimal_age_comment: str
+    lay_chronological_decimal_age_comment: str
+
+class ChronologicalDecimalAgeData(BaseModel):
+    x: float
+    y: float
+    observation_error: Optional[str]
+    age_type: Literal["chronological_age", "corrected_age"]
+    calendar_age: str
+    lay_comment: str
+    clinician_comment: str
+    age_error: Optional[str]
+    centile_band: str
+    observation_value_error: Optional[str]
+class CorrectedDecimalAgeData(BaseModel):
+    x: float
+    y: float
+    observation_error: Optional[str]
+    age_type: Literal["chronological_age", "corrected_age"]
+    calendar_age: str
+    lay_comment: str
+    clinician_comment: str
+    age_error: Optional[str]
+    centile_band: str
+    observation_value_error: Optional[str]
+
+class CentileData(BaseModel):
+    chronological_decimal_age_data: ChronologicalDecimalAgeData
+    corrected_decimal_age_data: CorrectedDecimalAgeData
+
+class SDSData(BaseModel):
+    chronological_decimal_age_data: ChronologicalDecimalAgeData
+    corrected_decimal_age_data: CorrectedDecimalAgeData
+
+class PlottableData(BaseModel):
+    centile_data: CentileData
+    sds_data: SDSData
+class BirthData(BaseModel):
+    birth_date: date
+    gestation_weeks: int
+    gestation_days: int
+    estimated_date_delivery: str
+    estimated_date_delivery_string: str
+    sex: Literal['male', 'female']
+
+
+class MeasurementDates(BaseModel):
+    observation_date: date
+    chronological_decimal_age: float
+    corrected_decimal_age: float
+    chronological_calendar_age: str
+    corrected_calendar_age: str
+    corrected_gestational_age: CorrectedGestationalAge
+    comments: Comments
+    corrected_decimal_age_error: Optional[str]
+    chronological_decimal_age_error: Optional[str]
+
+class ChildObservationValue(BaseModel):
+    measurement_method: Literal['height', 'weight', 'ofc', 'bmi']
+    observation_value: float
+    observation_value_error: Optional[str]
+
+class MeasurementCalculatedValues(BaseModel):
+    corrected_sds: float
+    corrected_centile: float
+    corrected_centile_band: str
+    chronological_sds: float
+    chronological_centile: float
+    chronological_centile_band: str
+    corrected_measurement_error: Optional[str]
+    chronological_measurement_error: Optional[str]
+    corrected_percentage_median_bmi: float
+    chronological_percentage_median_bmi: float
+
+class MeasurementObject(BaseModel):
+    birth_data: BirthData
+    measurement_dates: MeasurementDates
+    child_observation_value: ChildObservationValue
+    measurement_calculated_values: MeasurementCalculatedValues
+    plottable_data: PlottableData
