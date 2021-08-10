40d64577bac2d28dde5f0ad0d4b5c85f4322bdb7
Marcus Baw <marcusbaw@gmail.com>
2021-08-10 10:24:12 +0100

    adds a working test of the of the apispec endpoint at `/` which compares to a file in the tests directory.
    fixes the apispec file-save behaviour
    
    Also: removes YAML spec as it's not really needed and is additional code to maintain

--- tests/test_openapi_spec.py
+++ tests/test_openapi_spec.py
@@ -5,10 +5,21 @@ Tests for the openAPI spec endpoint
 # third-party imports
 from main import app
 from fastapi.testclient import TestClient
-import pytest
+import requests
+import json
 
 client = TestClient(app)
 
 def test_read_main():
+    
+    # get the root API endpoint (returns JSON openAPI3 spec)
     response = client.get("/")
+    
+    # load the API spec from the static saved test APIspec version
+    with open(r'tests/test_openapi.json', 'r') as file:
+        apispec = file.read()
+    
     assert response.status_code == 200
+    
+    # load the two JSON responses as Python Dicts so enable comparison (slow but more reliable)
+    assert response.json() == json.loads(apispec)
