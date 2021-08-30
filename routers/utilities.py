ad5b39e829c6f3c479af015f9485bff97075421d
Simon Chapman <eatyourpeasapps@gmail.com>
2021-08-30 12:24:16 +0100

    update utilities tag

--- routers/utilities.py
+++ routers/utilities.py
@@ -15,7 +15,7 @@ utilities = APIRouter(
 )
 
 
-@utilities.post('/mid-parental-height', tags='utilities')
+@utilities.post('/mid-parental-height', tags=['utilities'])
 def mid_parental_height_endpoint(mid_parental_height_request: MidParentalHeightRequest):
     """
     ## Mid-parental-height Endpoint
