0ccef6a476ebb880d03790d04eebb7b8af3b3e6e
Simon Chapman <eatyourpeasapps@gmail.com>
2022-05-15 00:14:05 +0100

    Bump version: 4.2.12 → 4.2.13

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from rcpchgrowth import chart_functions, constants
 from routers import trisomy_21, turners, uk_who, utilities
 
 
-version='4.2.12'  # this is set by bump version
+version='4.2.13'  # this is set by bump version
 
 # Declare the FastAPI app
 app = FastAPI(
